26a1be85b82cf06e377c9b2c0f4b0dbadf3b2a99
Jeremy Kanter <jeremykanter@gmail.com>
2025-06-06 13:40:48 -0400

    Initial Commit

---
+++ SGNS/SGNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SGNSApp.swift
+//  SGNS
+//
+//  Created by Jeremy Kanter on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SGNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SGNSTests/SGNSTests.swift
@@ -0,0 +1,16 @@
+//
+//  SGNSTests.swift
+//  SGNSTests
+//
+//  Created by Jeremy Kanter on 6/6/25.
+//
+
+import Testing
+
+struct SGNSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
